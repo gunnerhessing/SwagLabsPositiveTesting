ad15f306377136cf81c915d0131908a98bf43aa6
gunnerhessing <144730639+gunnerhessing@users.noreply.github.com>
2023-11-28 13:12:23 -0700

    Add files via upload

---
+++ positivetesting/test/specs/test.e2e.js
@@ -0,0 +1,15 @@
+const { expect } = require('@wdio/globals')
+const LoginPage = require('../pageobjects/login.page')
+const SecurePage = require('../pageobjects/secure.page')
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open()
+
+        await LoginPage.login('standard_user', 'secret_sauce')
+        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'Sauce Labs Bike Light')
+    })
+})
+
